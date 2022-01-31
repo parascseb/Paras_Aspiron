e74ee6ba62713fb6139dfd1cadd9e644d46a5e91
parascseb <91192621+parascseb@users.noreply.github.com>
2022-01-31 11:25:40 +0530

    Add files via upload

---
+++ src/Flights.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Flights(){
+    return(
+        
+            <div>
+                <h1>This is flights</h1>
+            </div>
+        
+    )
+}
+export default Flights;
